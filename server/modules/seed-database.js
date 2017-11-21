149c11e9a5dafec4bc855c4accd4e27f6f3278a1
Jenny Li <jennyzhcl@yahoo.com>
2017-11-21 15:47:45 -0800

    removed recently read, media, and links sections in profile

--- server/modules/seed-database.js
+++ server/modules/seed-database.js
@@ -16,21 +16,6 @@ let _seedUsers = () => {
           'Pro-life',
           'DACA',
           'Scientology'
-        ],
-        links: [
-          'https://www.youtube.com/watch?v=4VwElW7SbLA',
-          'https://www.youtube.com/watch?v=wsUhyiO31DY'
-        ],
-        recent: [
-          'https://www.nytimes.com/2017/11/02/us/politics/tax-plan-republicans.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=a-lede-package-region&region=top-news&WT.nav=top-news',
-          'https://www.nytimes.com/2017/11/02/your-money/tax-plan-changes-predict.html?action=click&contentCollection=Politics&module=RelatedCoverage&region=Marginalia&pgtype=article'
-        ],
-        badges: [
-          'chat-pro',
-          'top-reviewed'
-        ],
-        testimonials: [
-          'Open-minded and willing to listen to others. Highly recommended! - Steve'
         ]
       },
       roles: [ 'admin' ]
@@ -47,21 +32,6 @@ let _seedUsers = () => {
           'Affordable Health Care',
           'Climate Change',
           'DACA'
-        ],
-        links: [
-          'https://www.youtube.com/watch?v=LUvDfvZVRck',
-          'https://www.youtube.com/watch?v=udrKnXueTW0'
-        ],
-        recent: [
-          'http://www.cnn.com/2017/11/03/politics/trump-swamp-tax-reform/index.html',
-          'http://www.cnn.com/2017/11/02/politics/elizabeth-warren-democratic-party/index.html'
-        ],
-        badges: [
-          'respected-voice',
-          'five-month-active-user'
-        ],
-        testimonials: [
-          'Well-respected and a great person to talk to! - Joe'
         ]
       },
       roles: [ 'admin' ]
@@ -78,21 +48,6 @@ let _seedUsers = () => {
           'Tax Reform',
           'Immigration Reform',
           'Trees'
-        ],
-        links: [
-          'https://www.youtube.com/watch?v=xnW9uEWZyPc',
-          'https://www.youtube.com/watch?v=zmhkZ-pdQTg'
-        ],
-        recent: [
-          'https://www.washingtonpost.com/business/economy/the-gops-bill-is-a-sensible-framework--but-still-a-deficit-exploding-tax-cut-for-the-rich-and-corporations/2017/11/02/28b3688c-bffe-11e7-959c-fe2b598d8c00_story.html?hpid=hp_hp-top-table-main_taxanalysis-920pm%3Ahomepage%2Fstory&utm_term=.029b59aab84e',
-          'https://www.washingtonpost.com/business/economy/republican-tax-plan-to-lower-cap-on-mortgage-interest-deduction-to-500000-loans/2017/11/02/c0f594d6-bfd5-11e7-8444-a0d4f04b89eb_story.html?hpid=hp_hp-top-table-main_taxreform-1010am%3Ahomepage%2Fstory&utm_term=.27c1c4d0b378'
-        ],
-        badges: [
-          'great-reputation',
-          'chat-pro'
-        ],
-        testimonials: [
-          'Makes a strong effort to build empathy with the people he talks to. Enjoyable conversation! - Paul'
         ]
       },
       roles: [ 'admin' ],
@@ -110,19 +65,6 @@ let _seedUsers = () => {
           'DACA',
           'Job growth',
           'Pro-choice'
-        ],
-        links: [
-          'https://www.youtube.com/watch?v=XHDbYQst9t0'
-        ],
-        recent: [
-          'http://www.latimes.com/politics/la-pol-ca-tax-cuts-reaction-20171103-story.html',
-          'http://www.dailycal.org/2017/11/02/uc-berkeley-cut-deficit-half-fiscal-years-2016-17-chancellor-announces/'
-        ],
-        badges: [
-          'free-speech-promoter'
-        ],
-        testimonials: [
-          'Needs to do more in terms of learning how to listen to the person they are talking to - Anonymous'
         ]
       },
       roles: [ 'admin' ]
@@ -139,19 +81,6 @@ let _seedUsers = () => {
           'Strong military',
           'Veteran care',
           'Affordable Health Care'
-        ],
-        links: [
-          'https://www.youtube.com/watch?v=GX8EU9MeyJw'
-        ],
-        recent: [
-          'https://www.wsj.com/articles/mr-under-the-radar-jerome-powell-trumps-federal-reserve-pick-signals-continuity-1509643306',
-          'https://www.wsj.com/articles/prosecutors-consider-bringing-charges-in-dnc-hacking-case-1509618203',
-         ],
-        badges: [
-          'top-reviewed'
-        ],
-        testimonials: [
-          'Once you give him a mission, he will see it through. Very dependable! - Anonymous'
         ]
       },
       roles: [ 'admin' ],
